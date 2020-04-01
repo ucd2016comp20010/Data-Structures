7b5828d147282f1632c1e22e3b0f61303cb59f00
Andrei <andrei.costin@ucdconnect.ie>
2020-03-31 21:41:30 -0400

    LinkedStack Completed with LinkedList from Java, not our project

--- src/projectCode20280/HeapPriorityQueue.java
+++ src/projectCode20280/HeapPriorityQueue.java
@@ -1,27 +1,37 @@
+/*
 package projectCode20280;
 
+*/
 /*
- */
+ *//*
+
 
 import java.util.ArrayList;
 import java.util.Comparator;
 
+*/
 /**
  * An implementation of a priority queue using an array-based heap.
- */
+ *//*
+
 
 public class HeapPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
 
-	/** Creates an empty priority queue based on the natural ordering of its keys. */
+	*/
+/** Creates an empty priority queue based on the natural ordering of its keys. *//*
+
   public HeapPriorityQueue() { super(); }
 
-  /**
+  */
+/**
    * Creates an empty priority queue using the given comparator to order keys.
    * @param comp comparator defining the order of keys in the priority queue
-   */
+   *//*
+
   public HeapPriorityQueue(Comparator<K> comp) { super(comp); }
 
-  /**
+  */
+/**
    * Creates a priority queue initialized with the respective
    * key-value pairs.  The two arrays given will be paired
    * element-by-element. They are presumed to have the same
@@ -29,7 +39,8 @@ public class HeapPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
    * the shorter of the arrays)
    * @param keys an array of the initial keys for the priority queue
    * @param values an array of the initial values for the priority queue
-   */
+   *//*
+
   public HeapPriorityQueue(K[] keys, V[] values) ;
 
   // protected utilities
@@ -39,52 +50,70 @@ public class HeapPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
   protected boolean hasLeft(int j);
   protected boolean hasRight(int j);
 
-  /** Exchanges the entries at indices i and j of the array list. */
+  */
+/** Exchanges the entries at indices i and j of the array list. *//*
+
   protected void swap(int i, int j) ;
 
-  /** Moves the entry at index j higher, if necessary, to restore the heap property. */
+  */
+/** Moves the entry at index j higher, if necessary, to restore the heap property. *//*
+
   protected void upheap(int j);
-  
-  /** Moves the entry at index j lower, if necessary, to restore the heap property. */
+
+  */
+/** Moves the entry at index j lower, if necessary, to restore the heap property. *//*
+
   protected void downheap(int j) ;
 
-  /** Performs a bottom-up construction of the heap in linear time. */
+  */
+/** Performs a bottom-up construction of the heap in linear time. *//*
+
   protected void heapify() ;
 
   // public methods
 
-  /**
+  */
+/**
    * Returns the number of items in the priority queue.
    * @return number of items
-   */
+   *//*
+
   @Override
   public int size() { return heap.size(); }
 
-  /**
+  */
+/**
    * Returns (but does not remove) an entry with minimal key.
    * @return entry having a minimal key (or null if empty)
-   */
+   *//*
+
   @Override
   public Entry<K,V> min() ;
 
-  /**
+  */
+/**
    * Inserts a key-value pair and return the entry created.
    * @param key     the key of the new entry
    * @param value   the associated value of the new entry
    * @return the entry storing the new key-value pair
    * @throws IllegalArgumentException if the key is unacceptable for this queue
-   */
+   *//*
+
   @Override
   public Entry<K,V> insert(K key, V value) throws IllegalArgumentException ;
 
-  /**
+  */
+/**
    * Removes and returns an entry with minimal key.
    * @return the removed entry (or null if empty)
-   */
+   *//*
+
   @Override
   public Entry<K,V> removeMin() ;
 
-  /** Used for debugging purposes only */
+  */
+/** Used for debugging purposes only *//*
+
   private void sanityCheck() {
     for (int j=0; j < heap.size(); j++) {
       int left = left(j);
@@ -97,3 +126,24 @@ public class HeapPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
   }
 }
 
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Entry<K, V> insert(K key, V value) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> min() {
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> removeMin() {
+        return null;
+    }
+}
+*/
--- src/projectCode20280/LinkedStack.java
+++ src/projectCode20280/LinkedStack.java
@@ -1,40 +1,65 @@
 package projectCode20280;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedStack<E> implements Stack<E> {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        LinkedStack<Integer> x = new LinkedStack<>();
 
-	}
+		// check if new stack is empty, should be true
+		System.out.println("Got: " + x.isEmpty() + ", expected: true");
 
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		// push numbers 1-10 to stack
+		for (int i = 0; i < 10; i++) {
+			x.push(i);
+		}
+		// test size, pop, size after pop, top
+		System.out.println("Size Got: " + x.size() + " Expected: 10");
+		System.out.println("Pop Got: " + x.pop() + " Expected: 9");
+		System.out.println("Size After Pop Got: " + x.size() + " Expected: 9");
+		System.out.println("Top Got: " + x.top() + " Expected: 8");
+    }
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    // list to store data
+    private LinkedList<E> list;
 
-	@Override
-	public void push(E e) {
-		// TODO Auto-generated method stub
-		
-	}
+    // constructor
+    public LinkedStack() {
+		list = new LinkedList<>();
+    }
 
-	@Override
-	public E top() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int size() {
+        return list.size();
+    }
 
-	@Override
-	public E pop() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+       list.addFirst(e);
+    }
+
+    @Override
+    public E top() {
+        if (!isEmpty()) {
+            // returns first element
+            return list.get(0);
+        }
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) {
+        	return null;
+		}
+        return list.removeFirst();
+    }
 
 }
